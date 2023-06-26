3910a1fc3cb8daddb13cd16e6e3ce65b6a2da31e
stephy stephen <stephystephen03@gmail.com>
2023-06-26 19:39:17 +1000

    created and displayed birthday list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+import data from "./data";
+import List from "./List";
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        
+      </section>
+    </main>
+  );
 };
 export default App;
---
+++ src/Person.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Person = ({ name, image, age }) => {
+  return (
+    <article>
+      <img src={image} alt={name} className="img" />
+      <div>
+        <h4>{name}</h4>
+        <p>{age}</p>
+      </div>
+    </article>
+  );
+};
+
+export default Person;
